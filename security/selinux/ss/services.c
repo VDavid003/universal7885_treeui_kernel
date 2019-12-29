2dccc1642a38a297d1b8a1d2687edee38a43cd31
BlackMesa123 <brother12@hotmail.it>
2019-12-29 10:18:20 +0530

    selinux: mega switch
    Signed-off-by: prashantpaddune <padadunep@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -774,7 +774,7 @@ out:
 	kfree(t);
 
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 #if !defined(CONFIG_RKP_KDP)
 	selinux_enforcing = 1;
 #endif
@@ -1545,7 +1545,7 @@ out:
 	kfree(n);
 
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 #if !defined(CONFIG_RKP_KDP)
 	selinux_enforcing = 1;
 #endif
@@ -1843,7 +1843,7 @@ static inline int convert_context_handle_invalid_context(struct context *context
 	u32 len;
 
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 	selinux_enforcing = 1;
 #endif
 // ] SEC_SELINUX_PORTING_COMMON
